903a2fc2e1d7fdf727641b03d590e918eb44dccc
rachid elaid <rachidelaid@gmail.com>
2022-03-24 16:34:51 +0100

    adding tables

--- schema.sql
+++ schema.sql
@@ -41,3 +41,27 @@ ALTER TABLE animals
   ADD CONSTRAINT fk_owner
     FOREIGN KEY (owner_id)
     REFERENCES owners (id);
+
+--vets table
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY NOT NULL, 
+    name VARCHAR(100) NOT NULL, 
+    date_of_graduation DATE NOT NULL
+);
+
+--Specialization table
+CREATE TABLE specializations (
+    species_id INT NOT NULL,
+    vet_id INT NOT NULL,
+    FOREIGN KEY (species_id) REFERENCES species (id),
+    FOREIGN KEY (vet_id) REFERENCES vets (id) 
+);
+
+--Visit table
+CREATE TABLE visits (
+    animal_id INT NOT NULL,
+    vet_id INT NOT NULL,
+    date_of_visit DATE,
+    FOREIGN KEY (animal_id) REFERENCES animals (id),
+    FOREIGN KEY (vet_id) REFERENCES vets (id) 
+);
